376ec780100c43f938c0c60f61cdcbc124d89ddd
kenjipy <80217235+kenjipy@users.noreply.github.com>
2021-05-20 14:33:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const Discord = reuire('discord.js);
+const client = new Discord.client;
+                       
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message", msg=> {
+  if (messag.content === '!server') {
+  const embed = new MessageEmbed()
+  .setTitle('')
 
+
+client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('pong');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '!avatar') {
+    message.reply(message.author.displayAvatarURL());
+  }
+});
+
+
+client.login('token');

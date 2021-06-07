92ebad362e3855cb81de8a79398c29bfd9520130
kenjipy <80217235+kenjipy@users.noreply.github.com>
2021-06-07 13:32:57 +0200

    added new commands

--- index.js
+++ index.js
@@ -8,11 +8,31 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// servver commands not ready 
-client.on('message", msg=> {
-  if (messag.content === '!server') {
-  const embed = new MessageEmbed()
-  .setTitle('')
+let user;
+if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+} else {
+    user = message.`enter code here`author;
+}
+
+// user commmand (whois)
+const member = message.guild.member(!user);
+
+const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(message.author.avatarURL)
+    .addField(`${user.tag}`, `${user}`, true)
+    .addField("ID:", `${user.id}`, true)
+    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+    .addField("Status:", `${user.presence.status}`, true)
+    .addField("In Server", message.guild.name, true)
+    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+    .addField("Bot:", `${user.bot}`, true)
+    .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
+    .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
+    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+message.channel.send({embed});
 
 // ping commands
   
